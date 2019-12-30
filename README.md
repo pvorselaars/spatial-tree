8d5e7db7b27e5b30eb4bf9a9324a288150f191cb
Perry Vorselaars <p.vorselaars@gmail.com>
2019-12-30 18:19:04 +0100

    Implemented octree. Updated classes dot. Updated readme.

---
+++ README.md
@@ -0,0 +1,9 @@
+# spatial-tree
+
+A simple C++ template library that implements quadtrees and octrees for different classes and coordinate precision.
+
+## namespace
+
+![namespace](doc/namespace.png)
+
+
--- spatialtree.h
+++ spatialtree.h
@@ -1,42 +1,50 @@
 #include <vector>
 
+template <class T, class U>
 class Node {
 	public:
-	Node(float X, float Y, float Z, int d) : 
+	Node(U X, U Y, U Z, T d) : 
 		x(X), y(Y), z(Z), data(d) {};
-	float x;
-	float y;
-	float z;
-	int data;
+	U x;
+	U y;
+	U z;
+	T data;
 };
 
+template <class U>
 class AABB {
 	public:
-	AABB(float X, float Y, float Z, float W, float H, float D) : 
+	AABB(U X, U Y, U Z, U W, U H, U D) : 
 		x(X), y(Y), z(Z), w(W), h(H), d(D) {};
 
-	float x;
-	float y;
-	float z;
-	float w;
-	float h;
-	float d;
+	AABB() : x(0), y(0), z(0), w(10), h(10), d(10) {};
 
-	bool contains(float X, float Y, float Z){
+	AABB& operator=(AABB other){
+		x = other.x;
+		y = other.y;
+		z = other.z;
+		w = other.w;
+		h = other.h;
+		d = other.d;
+
+		return *this;
+	}
+
+	U x;
+	U y;
+	U z;
+	U w;
+	U h;
+	U d;
+
+	bool contains(U X, U Y, U Z) const {
 		
-		return (X >= x - w & X <= x + w &
-			Y >= y - h & Y <= y + h &
-			Z >= z - d & Z <= z + d);
+		return ((X >= x - w) && (X <= x + w) &&
+			(Y >= y - h) && (Y <= y + h) &&
+			(Z >= z - d) && (Z <= z + d));
 	}
 
-	bool contains(Node node){
-
-		return (node.x >= x - w & node.x <= x + w &
-			node.y >= y - h & node.y <= y + h &
-			node.z >= z - d & node.z <= z + d);
-	}
-
-	bool intersect(const AABB &other){
+	bool intersect(AABB other) {
 		
 		if ( abs(x - other.x) > (w + other.w) ) return false;
 		if ( abs(y - other.y) > (h + other.h) ) return false;
@@ -46,81 +54,143 @@ class AABB {
 	}
 };
 
+template <class T, class U>
 class QuadTree {
 	public:
-	QuadTree(AABB b) :
-		boundary(b) {
+	QuadTree<T,U>() : 
+		boundary(),
+		nodes(),
+		NW(),
+		NE(),
+		SE(),
+		SW(){};
+
+	QuadTree<T,U>(AABB<U> b) :
+		boundary(b),
+		nodes(),
+		NW(),
+		NE(),
+		SE(),
+		SW(){
 
 		nodes.reserve(capacity);
 	};
 
-	AABB boundary;
-	const int capacity = 4;
-	std::vector<Node> nodes;
+	QuadTree<T, U> (const QuadTree<T, U>& other) : boundary(other.boundary), 
+						      nodes(other.nodes),
+						      NW(),
+						      NE(),
+						      SE(),
+						      SW() {};
 
-	QuadTree * NW = nullptr;
-	QuadTree * NE = nullptr;
-	QuadTree * SE = nullptr;
-	QuadTree * SW = nullptr;
+	QuadTree<T, U>& operator=(QuadTree<T, U> other) {
+		boundary = other.boundary;
+		return *this;
+	}
+
+	~QuadTree(){
+		if(divided){
+			delete NW;
+			delete NE;
+			delete SE;
+			delete SW;
+			
+		}
+	}
+
+	AABB<U> boundary;
+	const long unsigned int capacity = 8;
+	std::vector<Node<T,U>> nodes;
+	bool divided = false;
+
+	QuadTree * NW;
+	QuadTree * NE;
+	QuadTree * SE;
+	QuadTree * SW;
 	
-	bool insert(Node node){
-		if(!boundary.contains(node))
+	bool insert(Node<T,U> node){
+		if(!boundary.contains(node.x, node.y, node.z))
 			return false;
 
 		if(nodes.size() < capacity){
 			nodes.push_back(node);
 			return true;
 		}else{
-			subdivide();
-
-			if(NW->insert(node)){
-				return true;
-			}else if(NE->insert(node)){
-				return true;
-			}else if(SE->insert(node)){
-				return true;
+			if(!divided && ((boundary.w  > 1 ) && (boundary.h > 1))){	
+				subdivide();
+			}else if(boundary.w <= 1){
+				return false;
 			}
-			
-			SW->insert(node);
 
-			return true;
+			if(NW->boundary.contains(node.x, node.y, node.z))
+				return NW->insert(node);
+
+			if(NE->boundary.contains(node.x, node.y, node.z))
+				return NE->insert(node); 
+
+			if(SE->boundary.contains(node.x, node.y, node.z))
+				return SE->insert(node);
+
+			if(SW->boundary.contains(node.x, node.y, node.z))
+				return SW->insert(node);
+
+		return false;
 		}	
 	}
 
-	void subdivide(){
-		AABB nw(boundary.x - boundary.w/2, boundary.y + boundary.h/2, 0, boundary.w/2, boundary.h/2, 0);
-		NW = new QuadTree(boundary);
-		AABB ne(boundary.x + boundary.w/2, boundary.y + boundary.h/2, 0, boundary.w/2, boundary.h/2, 0);
-		NE = new QuadTree(boundary);
-		AABB se(boundary.x + boundary.w/2, boundary.y - boundary.h/2, 0, boundary.w/2, boundary.h/2, 0);
-		SE = new QuadTree(boundary);
-		AABB sw(boundary.x - boundary.w/2, boundary.y - boundary.h/2, 0, boundary.w/2, boundary.h/2, 0);
-		SW = new QuadTree(boundary);
+	bool remove(T node){
+
+		if(nodes.size() == 0)
+			return false;
+
+		for(auto it = nodes.begin(); it != nodes.end(); it++){
+			if(it->data == node){
+				nodes.erase(it);
+				return true;
+			}
+				
+		}
+		return false;
 	}
 
-	std::vector<Node> query(AABB range){
-		std::vector<Node> found;
+	void subdivide(){
+
+		AABB<U> nw(boundary.x - boundary.w / 2, boundary.y - boundary.h / 2, 0, boundary.w/2, boundary.h/2, 0);
+
+		this->NW = new QuadTree<T,U>(nw);
+		
+		AABB<U> ne(boundary.x + boundary.w / 2, boundary.y + boundary.h / 2, 0, boundary.w/2, boundary.h/2, 0);
+
+		this->NE = new QuadTree<T,U>(ne);
+
+		AABB<U> se(boundary.x + boundary.w / 2, boundary.y - boundary.h / 2, 0, boundary.w/2, boundary.h/2, 0);
+
+		this->SE = new QuadTree<T,U>(se);
+
+		AABB<U> sw(boundary.x - boundary.w / 2, boundary.y + boundary.h / 2, 0, boundary.w/2, boundary.h/2, 0);
+
+		this->SW = new QuadTree<T,U>(sw);
+
+		divided = true;
+	}
+
+	std::vector<Node<T,U>> * query(AABB<U> range, std::vector<Node<T,U>> * found = new std::vector<Node<T,U>>()){
 
 		if (!boundary.intersect(range))
 			return found;
 
 		for(auto it = nodes.begin(); it != nodes.end(); it++){
-			if(boundary.contains(*it))
-				found.push_back(*it);
+			Node<T,U> n = *it;
+			if(range.contains(n.x, n.y, n.z))
+				found->push_back(n);
 		}
 
-		if(NW != nullptr){
+		if(divided){
 
-			auto nw = NW->query(range);
-			auto ne = NE->query(range);
-			auto se = SE->query(range);
-			auto sw = SW->query(range);
-			
-			found.insert(found.end(), nw.begin(), nw.end());
-			found.insert(found.end(), ne.begin(), ne.end());
-			found.insert(found.end(), se.begin(), se.end());
-			found.insert(found.end(), sw.begin(), sw.end());
-
+			NW->query(range, found);
+			NE->query(range, found);
+			SE->query(range, found);
+			SW->query(range, found);
 		}
 
 
@@ -128,6 +198,242 @@ class QuadTree {
 
 	}
 
+	void draw(sf::RenderWindow& window){
+
+		sf::RectangleShape b(sf::Vector2f(2*boundary.w, 2*boundary.h)*16.f);
+		b.setPosition(sf::Vector2f((boundary.x-boundary.w), (boundary.y-boundary.h))*16.f);
+		b.setFillColor(sf::Color::Transparent);
+		b.setOutlineThickness(1.f);
+		b.setOutlineColor(sf::Color(255,0,0));
+		window.draw(b);
+
+		if(NW != nullptr){
+			
+		NW->draw(window);
+		NE->draw(window);
+		SE->draw(window);
+		SW->draw(window);
+
+		}
+
+		
+		
+	} 
+
 };
 
+template <class T, class U>
+class OcTree {
+	public:
+	OcTree<T,U>() : 
+		boundary(),
+		nodes(),
+		NNW(),
+		NNE(),
+		NSE(),
+		NSW(),
+		SNW(),
+		SNE(),
+		SSE(),
+		SSW(){};
 
+	OcTree<T,U>(AABB<U> b) :
+		boundary(b),
+		nodes(),
+		NNW(),
+		NNE(),
+		NSE(),
+		NSW(),
+		SNW(),
+		SNE(),
+		SSE(),
+		SSW(){
+
+		nodes.reserve(capacity);
+	};
+
+	OcTree<T, U> (const OcTree<T, U>& other) : boundary(other.boundary), 
+						   nodes(other.nodes),
+						   NNW(),
+						   NNE(),
+						   NSE(),
+						   NSW(),
+						   SNW(),
+						   SNE(),
+						   SSE(),
+						   SSW(){};
+
+	OcTree<T, U>& operator=(OcTree<T, U> other) {
+		boundary = other.boundary;
+		return *this;
+	}
+
+	~OcTree(){
+
+		if(divided){
+			
+			delete NNE;
+			delete NSE;
+			delete NSW;
+			delete SNW;
+			delete SNE;
+			delete SSE;
+			delete SSW;
+
+		}
+	}
+
+	AABB<U> boundary;
+	const long unsigned int capacity = 8;
+	std::vector<Node<T,U>> nodes;
+	bool divided = false;
+
+	OcTree * NNW;
+	OcTree * NNE;
+	OcTree * NSE;
+	OcTree * NSW;
+	OcTree * SNW;
+	OcTree * SNE;
+	OcTree * SSE;
+	OcTree * SSW;
+	
+	bool insert(Node<T,U> node){
+		if(!boundary.contains(node.x, node.y, node.z))
+			return false;
+
+		if(nodes.size() < capacity){
+			nodes.push_back(node);
+			return true;
+		}else{
+			if(!divided && ((boundary.w  > 1 ) && (boundary.h > 1))){	
+				subdivide();
+			}else if(boundary.w <= 1){
+				return false;
+			}
+
+			if(NNW->boundary.contains(node.x, node.y, node.z))
+				return NNW->insert(node);
+
+			if(NNE->boundary.contains(node.x, node.y, node.z))
+				return NNE->insert(node);
+
+			if(NSE->boundary.contains(node.x, node.y, node.z))
+				return NSE->insert(node);
+
+			if(NSW->boundary.contains(node.x, node.y, node.z))
+				return NSW->insert(node);
+
+			if(SNW->boundary.contains(node.x, node.y, node.z))
+				return SNW->insert(node); 
+
+			if(SNE->boundary.contains(node.x, node.y, node.z))
+				return SNE->insert(node);
+
+			if(SSE->boundary.contains(node.x, node.y, node.z))
+				return SSE->insert(node);
+
+			if(SSW->boundary.contains(node.x, node.y, node.z))
+				return SSW->insert(node);
+
+		return false;
+
+		}	
+	}
+
+	bool remove(Node<T,U> node){
+
+		if(!boundary.contains(node.x, node.y, node.z))
+			return false;
+
+		if(nodes.size() == 0)
+			return false;
+		
+		for(auto it = nodes.begin(); it != nodes.end(); it++){
+			if(it->data == node.data){
+				nodes.erase(it);
+				return true;
+			}
+				
+		}
+
+		NNW->remove(node);
+		NNE->remove(node);
+		NSE->remove(node);
+		NSW->remove(node);
+		SNW->remove(node);
+		SNE->remove(node);
+		SSE->remove(node);
+		SSW->remove(node);
+		
+		return true;
+
+
+	}
+
+	void subdivide(){
+
+		AABB<U> nnw(boundary.x - boundary.w / 2, boundary.y - boundary.h / 2, boundary.z - boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->NNW = new OcTree<T,U>(nnw);
+		
+		AABB<U> nne(boundary.x + boundary.w / 2, boundary.y + boundary.h / 2, boundary.z - boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->NNE = new OcTree<T,U>(nne);
+
+		AABB<U> nse(boundary.x + boundary.w / 2, boundary.y - boundary.h / 2, boundary.z - boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->NSE = new OcTree<T,U>(nse);
+
+		AABB<U> nsw(boundary.x - boundary.w / 2, boundary.y + boundary.h / 2, boundary.z - boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d / 2);
+
+		this->NSW = new OcTree<T,U>(nsw);
+
+		AABB<U> snw(boundary.x - boundary.w / 2, boundary.y - boundary.h / 2, boundary.z + boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->SNW = new OcTree<T,U>(snw);
+		
+		AABB<U> sne(boundary.x + boundary.w / 2, boundary.y + boundary.h / 2, boundary.z + boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->SNE = new OcTree<T,U>(sne);
+
+		AABB<U> sse(boundary.x + boundary.w / 2, boundary.y - boundary.h / 2, boundary.z + boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d/2);
+
+		this->SSE = new OcTree<T,U>(sse);
+
+		AABB<U> ssw(boundary.x - boundary.w / 2, boundary.y + boundary.h / 2, boundary.z + boundary.d / 2, boundary.w/2, boundary.h/2, boundary.d / 2);
+
+		this->SSW = new OcTree<T,U>(ssw);
+
+		divided = true;
+	}
+
+	std::vector<Node<T,U>> * query(AABB<U> range, std::vector<Node<T,U>> * found = new std::vector<Node<T,U>>()){
+
+		if (!boundary.intersect(range))
+			return found;
+
+		for(auto it = nodes.begin(); it != nodes.end(); it++){
+			Node<T,U> n = *it;
+			if(range.contains(n.x, n.y, n.z))
+				found->push_back(n);
+		}
+
+		if(divided){
+
+			NNW->query(range, found);
+			NNE->query(range, found);
+			NSE->query(range, found);
+			NSW->query(range, found);
+			SNW->query(range, found);
+			SNE->query(range, found);
+			SSE->query(range, found);
+			SSW->query(range, found);
+		}
+
+
+		return found;
+
+	}
+
+
+};
