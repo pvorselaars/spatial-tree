84348e03d5e151ee011a4fe4fe07e8938d855687
Perry Vorselaars <perry@perigee.xyz>
2021-01-21 15:15:05 +0100

    Updated tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,7 +8,15 @@ TEST_CASE("vector arithmetic", "[Vector]"){
 	Vector<int> v1(0,1,0);
 	Vector<int> v2(1,0,1);
 	Vector<int> v3(1,1,1);
+	Vector<int> v4(4,4,4);
 
-	REQUIRE( v1 + v2 == v3 );
-	REQUIRE( v3 - v1 == v2 );
+	SECTION("vector addition"){
+		REQUIRE( v1 + v2 == v3 );
+	}
+	SECTION("vector subtraction"){
+		REQUIRE( v3 - v1 == v2 );
+	}
+	SECTION("vector multiplication"){
+		REQUIRE( v3 * 4 == v4 );
+	}
 }
